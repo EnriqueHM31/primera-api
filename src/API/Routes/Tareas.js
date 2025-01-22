97eba20d3d24492fc052238472aba1e6615efc50
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-21 21:39:59 -0600

    Condiciones para verificar que el cuerpo venga completo

--- src/API/Routes/Tareas.js
+++ src/API/Routes/Tareas.js
@@ -7,8 +7,36 @@ router.get('/', (_, res) => {
     res.send(ServicesTareas.getTareasSinPrioridad())
 })
 
-router.post('/', (_, res) => {
-    res.send('Hola posting')
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    const tarea = ServicesTareas.getTareasID(Number(id)) // Asumiendo que esta función busca la tarea por ID
+
+    if (!tarea) {
+        return res.status(404).send([{ error: 'Tarea no encontrada' }])
+    }
+
+    res.send(tarea)
+})
+
+router.post('/', (req, res) => {
+    if (req.body === undefined) {
+        return res.status(400).send({ error: 'No se recibieron datos' })
+    }
+
+    const { nombre, descripcion, prioridad, fecha } = req.body
+
+    if (!nombre || !descripcion || !prioridad || !fecha) {
+        return res.status(400).send('Todos los campos son obligatorios')
+    }
+
+    const tarea = ServicesTareas.postTareas({
+        nombre,
+        descripcion,
+        prioridad,
+        fecha,
+    })
+
+    res.json(tarea)
 })
 
 export default router
