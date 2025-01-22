1cf21b883363e0b1c11c495c2e205dcda810484f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-21 20:28:43 -0600

    modificacion de las routes

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import router from './Routes/Tareas.js'
 
 const app = express()
 
@@ -7,6 +8,8 @@ const PORT = 3000
 app.get('/', (_req, res) => {
     res.send('Estoy usando una api verdad funciona ya verdad')
 })
+
+app.use('/api/tareas', router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
